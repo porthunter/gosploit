2d21af5b3da0ef76abe6bdc9656a2734eb7ce7b3
porthunter <porthunter@tuta.io>
2018-02-11 00:30:44 +0100

    code

--- gosploit.go
+++ gosploit.go
@@ -101,6 +101,38 @@ func main() {
 	text, _ := reader.ReadString('\n')
 	//fmt.Println(text)
 
+	lines, err := readLines("../xs2pwn/domains.txt")
+    if err != nil {
+		fmt.Println(text)
+    }
+
+
+
+	
+
+    // In order to use our pool of workers we need to send
+    // them work and collect their results. We make 2
+    // channels for this.
+    jobs := make(chan int, 100)
+    results := make(chan int, 100)
+
+    // This starts up 3 workers, initially blocked
+    // because there are no jobs yet.
+    for w := 1; w <= 200; w++ {
+        go worker(w, jobs, results)
+    }
+
+    // Here we send 5 `jobs` and then `close` that
+    // channel to indicate that's all the work we have.
+    for j := 1; j <= len(lines); j++ {
+        jobs <- j
+    }
+    close(jobs)
+
+
+
+
+
 	var mod string
 	switch text {
 	case "english\n":
@@ -132,30 +164,6 @@ func main() {
 	if !ok {
 		fmt.Println("unexpected type from module symbol")
 	}
-
-    lines, err := readLines("../xs2pwn/domains.txt")
-    if err != nil {
-		fmt.Println(text)
-    }
-
-    // In order to use our pool of workers we need to send
-    // them work and collect their results. We make 2
-    // channels for this.
-    jobs := make(chan int, 100)
-    results := make(chan int, 100)
-
-    // This starts up 3 workers, initially blocked
-    // because there are no jobs yet.
-    for w := 1; w <= 200; w++ {
-        go worker(w, jobs, results)
-    }
-
-    // Here we send 5 `jobs` and then `close` that
-    // channel to indicate that's all the work we have.
-    for j := 1; j <= len(lines); j++ {
-        jobs <- j
-    }
-    close(jobs)
 
 	// 4. use the module
 	module.Exploit()
