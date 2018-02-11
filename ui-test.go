2a59fd719f87fa23cb9463e8844ff372c5f4ead7
porthunter <porthunter@tuta.io>
2018-02-11 12:05:43 +0100

    code

--- ui-test.go
+++ ui-test.go
@@ -1,32 +1,19 @@
 package main
 
-import ui "github.com/gizak/termui" // <- ui shortcut, optional
+import ui "github.com/gizak/termui"
 
-	func main() {
-		err := ui.Init()
-		if err != nil {
-			panic(err)
-		}
-		defer ui.Close()
+func main() {
+    if err:=ui.Init(); err != nil {
+        panic(err)
+    }
+    defer ui.Close()
 
-		p := ui.NewPar(":PRESS q TO QUIT DEMO")
-		p.Height = 3
-		p.Width = 50
-		p.TextFgColor = ui.ColorWhite
-		p.BorderLabel = "Text Box"
-		p.BorderFg = ui.ColorCyan
+    g := ui.NewGauge()
+    g.Percent = 50
+    g.Width = 50
+    g.BorderLabel = "Gauge"
 
-		g := ui.NewGauge()
-		g.Percent = 50
-		g.Width = 50
-		g.Height = 3
-		g.Y = 11
-		g.BorderLabel = "Gauge"
-		g.BarColor = ui.ColorRed
-		g.BorderFg = ui.ColorWhite
-		g.BorderLabelFg = ui.ColorCyan
+    ui.Render(g)
 
-		ui.Render(p, g) // feel free to call Render, it's async and non-block
-
-		// event handler...
-	}
+    ui.Loop()
+}
