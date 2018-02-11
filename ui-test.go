1592c90f52a4697159b39f34df669e566db4ac6f
porthunter <porthunter@tuta.io>
2018-02-11 12:10:12 +0100

    code

--- ui-test.go
+++ ui-test.go
@@ -8,12 +8,19 @@ func main() {
     }
     defer ui.Close()
 
-    g := ui.NewGauge()
-    g.Percent = 50
-    g.Width = 50
-    g.BorderLabel = "Gauge"
+	bc := termui.NewBarChart()
+	data := []int{3, 2, 5, 3, 9, 5}
+	bclabels := []string{"S0", "S1", "S2", "S3", "S4", "S5"}
+	bc.BorderLabel = "Bar Chart"
+	bc.Data = data
+	bc.Width = 26
+	bc.Height = 10
+	bc.DataLabels = bclabels
+	bc.TextColor = termui.ColorGreen
+	bc.BarColor = termui.ColorRed
+	bc.NumColor = termui.ColorYellow
 
-    ui.Render(g)
+    ui.Render(bc)
 
     ui.Loop()
 }
