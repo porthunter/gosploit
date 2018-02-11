3b6d710556d8955d55ae268781607ba94e2cc519
porthunter <porthunter@tuta.io>
2018-02-11 11:26:04 +0100

    code

--- gosploit.go
+++ gosploit.go
@@ -9,6 +9,8 @@ import (
     "os"
 	"github.com/fatih/color"
     "./engine"
+	"plugin"
+	"strings"
 )
 
 var lines []string
@@ -101,9 +103,46 @@ func main() {
 
 	//Get Module to Load
 	reader := bufio.NewReader(os.Stdin)
-	mod, _ := reader.ReadString('\n')
+	modpathstring, _ := reader.ReadString('\n')
 
 	//Run Module
-	engine.LoadModule(mod)
+	//engine.LoadModule(mod)
+
+	modulepath := strings.TrimSuffix(modpathstring, "\n")
+	var mod string
+
+	switch modulepath {
+	case "test/chi/chi":
+		mod = "./modules/test/chi/chi.so"
+	case "test/eng/eng":
+		mod = "./modules/test/eng/eng.so"
+	default:
+		fmt.Println("can't find module")
+	}
+
+	// load module
+	// 1. open the so file to load the symbols
+	plug, err := plugin.Open(mod)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 2. look up a symbol (an exported function or variable)
+	// in this case, variable GosploitModule
+	symGosploitModule, err := plug.Lookup("GosploitModule")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 3. Assert that loaded symbol is of a desired type
+	// in this case interface type GosploitModule (defined above)
+	var module GosploitModule
+	module, ok := symGosploitModule.(GosploitModule)
+	if !ok {
+		fmt.Println("unexpected type from module symbol")
+	}
+
+	// 4. use the module
+	module.Exploit()
 
 }
